83d4088ecb2c8b3889f3764bf29b99a6a6797f58
bransoned <bransonmb@proton.me>
2025-12-23 20:50:56 +0000

    #26 remove dependency functions from script.py

--- script.py
+++ script.py
@@ -14,72 +14,7 @@ import os
 
 from src.exceptions import *
 from src.validators import *
-
-# =========================================================================== #
-
-"""
-Find exiftool executable
-
-Returns:
-    Path to exiftool
-
-Raises:
-    DependencyError: If exiftool cannot be found
-"""
-def find_exiftool() -> str:
-
-    exe_name = "exiftool.exe" if sys.platform.startswith("win") else "exiftool"
-
-    # If bundle
-    if getattr(sys, "frozen", False):
-        base = Path(sys._MEIPASS)
-        bundled = base / "bin" / exe_name
-        if bundled.exists():
-            return str(bundled)
-
-    # Try system PATH
-    system_path = shutil.which(exe_name)
-    if system_path:
-        return system_path
-
-    raise DependencyError(
-        "Exiftool not found. Please install exiftool or use the provided bundled executable."
-        "For further installation instructions, reference the README."
-    )
-
-# =========================================================================== #
-
-"""
-Find ffmpeg executable.
-
-Returns:
-    Path to ffmpeg
-
-Raises:
-    DependencyError: If ffmpeg cannot be found
-"""
-def find_ffmpeg() -> str:
-
-    exe_name = "ffmpeg.exe" if sys.platform.startswith("win") else "ffmpeg"
-
-    # If bundle
-    if getattr(sys, "frozen", False):
-        base = Path(sys._MEIPASS)
-        bundled = base / "bin" / exe_name
-        if bundled.exists():
-            return str(bundled)
-
-    # Try system PATH
-    system_path = shutil.which(exe_name)
-    if system_path:
-        return system_path
-
-    raise DependencyError(
-        "FFMpeg not found. Please install ffmpeg or use the provided bundled executable."
-        "For further installation instructions, reference the README."
-    )
-
-# =========================================================================== #
+from src.dependencies import *
 
 """
 Parse user-provided HTML file for user-specific image info
